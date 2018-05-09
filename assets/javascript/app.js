fa59ea578e163a48c6e1ee47b4239c0f7f0ef066
Okens <Ng.Eric314@gmail.com>
2018-05-09 01:07:08 -0700

    Correcting issue where app.js was not loading properly. app.js needs to be loaded at the end, or use .ready.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 
 
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAWW6B8YEPyrIGKKIAN6IgPgxaYzvljJBA",
     authDomain: "train-scheduler-4a634.firebaseapp.com",
     databaseURL: "https://train-scheduler-4a634.firebaseio.com",
     projectId: "train-scheduler-4a634",
     storageBucket: "",
     messagingSenderId: "1067029497636"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
-$("#submitBtn").on("click", function(event){
-
+$("#submitBtn").on("click", function (event) {
+ 
     event.preventDefault();
 
     var trainName = $("#inputName").val();
@@ -23,17 +23,28 @@ $("#submitBtn").on("click", function(event){
     var trainFirstTime = $("#inputFirstTime").val();
     var trainFreq = $("#inputFreq").val();
 
+    console.log(trainName);
 
+
+    var trainData = {
+        name: trainName,
+        dest: trainDest,
+        firstTime: trainFirstTime,
+        freq: trainFreq
+    };
+
+    database.ref().push(trainData);
 
 });
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
     console.log(childSnapshot.val());
 
-    var trainName = childSnapshot.val().trainName;
-    var trainDest = childSnapshot.val().trainDest;
-    var trainFirstTime = childSnapshot.val().trainFirstTime;
-    var trainFreq = childSnapshot.val().trainFreq;
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().dest;
+    var trainFirstTime = childSnapshot.val().firstTime;
+    var trainFreq = childSnapshot.val().freq;
 
     var tableRow = $("<tr>");
     var tableNameTag = $("<td>");
